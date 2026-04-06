0ef1a950176347e29946d5ed29c2b8054bc27b29
Kiran Panchavati <kiranpanchavati9@gmail.com>
2026-04-06 20:24:55 +0400

    terraform v1 and v3 updated

---
+++ main.tf
@@ -0,0 +1,36 @@
+# Create a new tag for the user
+resource "digitalocean_tag" "tags" {
+  for_each = var.components
+  name = var.components[count.index]
+}
+
+# Create a new Droplet in nyc3 with the user tag
+resource "digitalocean_droplet" "instances" {
+  for_each = var.components
+  image  = var.image
+  name   = "${var.components[count.index]}-dev"
+  region = var.region
+  size   = var.size
+  tags   = each.key
+}
+
+## FIREWALL RULE ALLOW ALL FOR THE USER
+
+resource "digitalocean_firewall" "firewallrecords" {
+  name = "${each.key]}-fw"
+  for_each = var.components
+
+  droplet_ids = [digitalocean_droplet.instances[each.key].id]
+
+  inbound_rule {
+    protocol         = var.protocol
+    port_range       = var.port_range
+    source_addresses = var.source_address
+  }
+
+  outbound_rule {
+    protocol              = var.protocol
+    port_range            = var.port_range
+    destination_addresses = var.destination_addresses
+  }
+}
